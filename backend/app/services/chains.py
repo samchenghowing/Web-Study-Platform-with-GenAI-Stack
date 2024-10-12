402cf94e5ae42758e91bc34cd6423643b6f85611
samcheng <samchenghowing@gmail.com>
2024-10-12 23:08:41 +0800

    Remove google/ openai/ hugging face intergration

--- backend/app/services/chains.py
+++ backend/app/services/chains.py
@@ -1,12 +1,6 @@
-from langchain_openai import OpenAIEmbeddings
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.embeddings import OllamaEmbeddings
-from langchain_community.embeddings import BedrockEmbeddings
 
-from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
-from langchain_community.chat_models import BedrockChat
 
 from langchain_community.vectorstores import Neo4jVector
 
@@ -32,62 +26,25 @@ from typing import Optional, List, Any
 from config import BaseLogger
 
 def load_embedding_model(embedding_model_name: str, logger=BaseLogger(), config={}):
-    if embedding_model_name == "ollama":
-        embeddings = OllamaEmbeddings(
-            base_url=config["ollama_base_url"], model="llama3.1"
-        )
-        dimension = 2304
-        logger.info("Embedding: Using Ollama")
-    elif embedding_model_name == "openai":
-        embeddings = OpenAIEmbeddings()
-        dimension = 1536
-        logger.info("Embedding: Using OpenAI")
-    elif embedding_model_name == "aws":
-        embeddings = BedrockEmbeddings()
-        dimension = 1536
-        logger.info("Embedding: Using AWS")
-    elif embedding_model_name == "google-genai-embedding-001":        
-        embeddings = GoogleGenerativeAIEmbeddings(
-            model="models/embedding-001"
-        )
-        dimension = 768
-        logger.info("Embedding: Using Google Generative AI Embeddings")
-    else:
-        embeddings = HuggingFaceEmbeddings(
-            model_name="all-MiniLM-L6-v2", cache_folder="/embedding_model"
-        )
-        dimension = 384
-        logger.info("Embedding: Using SentenceTransformer")
+    embeddings = OllamaEmbeddings(
+        base_url=config["ollama_base_url"], model=embedding_model_name
+    )
+    dimension = 2304
+    logger.info("Embedding: Using ", embedding_model_name)
     return embeddings, dimension
 
 def load_llm(llm_name: str, logger=BaseLogger(), config={}):
-    if llm_name == "gpt-4":
-        logger.info("LLM: Using GPT-4")
-        return ChatOpenAI(temperature=0, model_name="gpt-4", streaming=True)
-    elif llm_name == "gpt-3.5":
-        logger.info("LLM: Using GPT-3.5")
-        return ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo", streaming=True)
-    elif llm_name == "claudev2":
-        logger.info("LLM: ClaudeV2")
-        return BedrockChat(
-            model_id="anthropic.claude-v2",
-            model_kwargs={"temperature": 0.0, "max_tokens_to_sample": 1024},
-            streaming=True,
-        )
-    elif len(llm_name):
-        logger.info(f"LLM: Using Ollama: {llm_name}")
-        return ChatOllama(
-            temperature=0,
-            base_url=config["ollama_base_url"],
-            model=llm_name,
-            streaming=True,
-            # seed=2,
-            top_k=10,  # A higher value (100) will give more diverse answers, while a lower value (10) will be more conservative.
-            top_p=0.3,  # Higher value (0.95) will lead to more diverse text, while a lower value (0.5) will generate more focused text.
-            num_ctx=3072,  # Sets the size of the context window used to generate the next token.
-        )
-    logger.info("LLM: Using GPT-3.5")
-    return ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo", streaming=True)
+    logger.info(f"LLM: Using Ollama: {llm_name}")
+    return ChatOllama(
+        temperature=0,
+        base_url=config["ollama_base_url"],
+        model=llm_name,
+        streaming=True,
+        # seed=2,
+        top_k=10,  # A higher value (100) will give more diverse answers, while a lower value (10) will be more conservative.
+        top_p=0.3,  # Higher value (0.95) will lead to more diverse text, while a lower value (0.5) will generate more focused text.
+        num_ctx=3072,  # Sets the size of the context window used to generate the next token.
+    )
 
 def configure_llm_only_chain(llm, CONN_STRING, DATABASE_NAME, COLLECTION_NAME):
     # Load chat history from MongoDB
